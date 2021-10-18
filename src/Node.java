d1590fc03c50e5910d2ee5f3e0bccf1bc7a3e9f5
Nondas Ioannou <nevwalkalone13@gmail.com>
2021-10-19 00:51:44 +0300

    little fixes

--- src/Node.java
+++ src/Node.java
@@ -1,34 +1,38 @@
+/**
+ * Class representing a Node, contains data and a reference to the next node.
+ * Generics were used.
+ */
 
 public class Node<T> {
-	private T data;
-	private Node<T> next;
-	
-	public Node(T data) {
-		this.data=data;
-		next=null;
-	}
-	
-	
-	/**
-    *@return the data of the specific node.
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        next = null;
+    }
+
+    /**
+     * @return the data of the specific node.
      */
-	public T getData() {
-		return data;
-	}
-	
-	/**
+    public T getData() {
+        return data;
+    }
+
+    /**
      * @return the reference to the next node.
      */
-	public Node<T> getNext() {
-		return next;
-	}
-	/**
+    public Node<T> getNext() {
+        return next;
+    }
+
+    /**
      * Sets the reference to the next node
-     *@param next
-     
+     * 
+     * @param next the node that will be referenced.
      */
-	public void setNext(Node<T> next) {
+    public void setNext(Node<T> next) {
         this.next = next;
     }
-	
+
 }
